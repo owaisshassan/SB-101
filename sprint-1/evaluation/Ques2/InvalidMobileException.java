b8c68bb2c0f1169908ce3904d2f28b847f3352b2
OwaissHassan <khabarquss@gmail.com>
2022-08-01 12:59:21 +0530

    ans2

---
+++ sprint-1/evaluation/Ques2/InvalidMobileException.java
@@ -0,0 +1,13 @@
+package sprint1evaluation.Ques2;
+
+public class InvalidMobileException extends Exception{
+
+	public InvalidMobileException(String errormsg){
+        super(errormsg);
+    }
+
+    public InvalidMobileException(){
+
+    }
+	
+}
--- sprint-1/evaluation/Ques2/Mobile.java
+++ sprint-1/evaluation/Ques2/Mobile.java
@@ -1,5 +1,51 @@
 package sprint1evaluation.Ques2;
 
-public class Mobile {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class Mobile {
+	  HashMap<String, ArrayList<String>> mobiles = new HashMap<>();
+
+
+	    public String addMobile(String company, String model){
+	        ArrayList<String> al = new ArrayList();
+	        al.add(model);
+	        mobiles.put(company,al);
+	        return "Mobile added successfully";
+	    }
+	    
+
+		 public List<String> getModels (String company) throws InvalidMobileException{
+
+		        return (List<String>) mobiles.keySet();
+
+		        
+		    }
+
+
+	    public static void main(String[] args) {
+
+	   Mobile m =new Mobile();
+	   m.addMobile("apple","iphone13");
+	   m.addMobile("samsung","iphone12");
+	   m.addMobile("ipnoe","iphone11");
+	   m.addMobile("galaxy","iphone10");
+
+	        System.out.println(m.mobiles);
+	        
+	        
+	        try {
+				System.out.println(m.getModels("apple"));
+			} catch (InvalidMobileException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+	    }
+
+	   
+	
 }
